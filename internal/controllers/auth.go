3792ffc7227cf32c0f07f54b29f3ad1d4465f4be
adityavarmachandika <aditya.chandika@techmojo.in>
2025-08-29 18:38:57 +0530

    signin function ✅

--- internal/controllers/auth.go
+++ internal/controllers/auth.go
@@ -1,11 +1,43 @@
 package controllers
 
 import (
+	"BlacAi/internal/db"
+	"BlacAi/internal/models"
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func SignupAuth(c *gin.Context){
-	c.JSON(http.StatusOK,gin.H{"status":"success"})
+
+	
+	SignupUserDataRaw,exist:= c.Get("SignupBody")
+
+	if !exist{
+
+		c.JSON(http.StatusBadRequest,gin.H{"error":"empty input data"})
+		return
+	}
+
+	SignupUserData,_:=SignupUserDataRaw.(models.UserSignupInput)
+
+
+	
+	//check for the email in the database 
+	user,err:= gorm.G[models.UserDetails](db.DB).Where("Email = ?", SignupUserData.Email).First(c.Request.Context())
+
+	if err!= nil{
+
+		if errors.Is(err,gorm.ErrRecordNotFound){
+			c.JSON(http.StatusOK,gin.H{"status":"user not found"})
+			return 
+		}
+		c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+		return
+	}
+	c.JSON(http.StatusNotFound, gin.H{"status":user})
+
+
 }
