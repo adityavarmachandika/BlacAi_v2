3ef5d323be17b5b1e81829a586d1ffabd8cdc402
adityavarmachandika <aditya.chandika@techmojo.in>
2025-09-04 11:42:57 +0530

    decoupling- GetUser

--- internal/repository/auth.go
+++ internal/repository/auth.go
@@ -1,12 +1,35 @@
 package repository
 
-import "BlacAi/internal/models"
+import (
+	"BlacAi/internal/db"
+	"BlacAi/internal/models"
+	"context"
+
+	"gorm.io/gorm"
+)
 
 type UserRepo interface {
 	GetUserByEmail(email string)(models.UserDetails,error)
 }
 
+type UserGormRepo struct{
+	db *gorm.DB
+}
 
-// func (r *UserRepo)GetUserByEmail(email string)(models.UserDetails,error){
 
-// }
+//---call this function in main ad give the db.DB as a parameter there 
+func NewUserRepo(db *gorm.DB) *UserGormRepo{
+	return &UserGormRepo{db:db}
+}
+
+//--fetches the user details based on the mail
+func (r *UserGormRepo)GetUserByEmail(email string,ctx context.Context)(models.UserDetails,error){
+
+	user,err:= gorm.G[models.UserDetails](db.DB).Where("Email = ?", email).First(ctx)
+
+	if err != nil{
+		return models.UserDetails{},err
+	}
+
+	return user,nil
+}
--- internal/service/auth.go
+++ internal/service/auth.go
@@ -1,7 +1,37 @@
 package service
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"BlacAi/internal/models"
+	"BlacAi/internal/repository"
+	"context"
 
+	"golang.org/x/crypto/bcrypt"
+)
+
+
+type userService struct {
+	Repo repository.UserRepo
+}
+
+//--call this function in the main after intializing the 
+func NewUserService(Repo repository.UserRepo)*userService{
+	return &userService{Repo:Repo}
+}
+
+
+
+func (u *userService)CreateUserAcc(user models.UserSignupInput,ctx context.Context)(bool,error){
+
+	UserDetails,err:= u.Repo.GetUserByEmail(user.Email)
+
+	if err !=nil{
+		return false,err
+	}else if UserDetails.Email==""{
+		return false,nil
+	}else{
+		return true,nil
+	}
+}
 
 
 
